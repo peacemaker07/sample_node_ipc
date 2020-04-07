86647fc357321b5af2488b7c9c2125859b2c742c
yujichino <chino@jsl.co.jp>
2020-04-07 19:21:56 +0900

    接続後、時間差でメッセージを送れるか試す

--- hello-client.js
+++ hello-client.js
@@ -6,7 +6,7 @@ ipc.config.retry = 10000;
 ipc.connectTo("testpipe", function() {
   ipc.of.testpipe.on("connect", function() {
     ipc.log("## connected to testpipe ##", ipc.config.delay);
-    ipc.of.testpipe.emit("app.message", "ping");
+    // ipc.of.testpipe.emit("app.message", "ping client1");
   });
   ipc.of.testpipe.on("disconnect", function() {
     ipc.log("disconnected from testpipe");
@@ -14,6 +14,17 @@ ipc.connectTo("testpipe", function() {
   ipc.of.testpipe.on("app.message", function(data) {
     ipc.log("got a message from testpipe : ", data);
   });
+  ipc.of.testpipe.on("app.message2", function(data) {
+    ipc.log("got a message from testpipe : ", data);
+  });
 
   console.log(ipc.of.testpipe.destroy);
 });
+
+setTimeout(function(){
+  ipc.of.testpipe.emit("app.message", "[message]ping client1");
+}, 5000);
+
+setTimeout(function(){
+  ipc.of.testpipe.emit("app.message2", "[message2]ping client1");
+}, 10000);
